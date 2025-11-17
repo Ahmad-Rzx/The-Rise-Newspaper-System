3943664ec5a2c38bd26944f8d5e57fc4b957ccc5
Syed Ahmad Raza <ahmadm2clan@gmail.com>
2025-11-17 17:59:44 +0500

    Refactor Graph structure for improved readability

--- Graph.h
+++ Graph.h
@@ -3,13 +3,11 @@
 #include<cstring>
 #pragma once
 using namespace std;
-
 struct Graph{
-    int** distances; //Stores Distances 
-    int numLocations; //Total Number of Locations
-    string* locations; // Array of Location Names
+    int** distances; 
+    int numLocations; 
+    string* locations; 
 };
-
 Graph* createGraph(int numLocations);
 void saveGraph(string filename, Graph*& graph);
 void loadGraph(string filename, Graph*& graph);
